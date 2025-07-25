0d1d8793a18d3fa96ce4bd1c575afb7d6f4f9bf7
Tim Matheis <tim.matheis@outlook.com>
2025-07-25 22:33:12 +0200

    Implemented cache time to live and max cache file count and made it configurable via the configuration file.

--- src/setup.py
+++ src/setup.py
@@ -4,16 +4,19 @@ import os
 import json
 
 cfg_folder_name: str = r".rainy"
+cache_folder_name: str = r"cache"
 cfg_file_name: str = r"config.ini"
 cache_file_name: str = r"cache.json"
-cache_ttl: int = 360
+
 
 class Config:
     def __init__(self):
         self.abs_home_path: str = str(os.path.expanduser("~"))
         self.abs_cfg_folder_path: str = os.path.join(self.abs_home_path, cfg_folder_name)
-        self.abs_cache_file_path: str = os.path.join(self.abs_cfg_folder_path, cache_file_name)
+        self.abs_cache_folder_path: str = os.path.join(self.abs_cfg_folder_path, cache_folder_name)
         self.abs_cfg_file_path: str = os.path.join(self.abs_cfg_folder_path, cfg_file_name)
+        self.cache_ttl: int = 360
+        self.max_cache_file_count: int = 10
 
         if not os.path.exists(self.abs_cfg_folder_path):
             self.create_cfg_folder()
@@ -22,6 +25,7 @@ class Config:
     def create_cfg_folder(self, reinit: bool = False):
         if reinit:
             self.remove_directory_tree(self.abs_cfg_folder_path)
+            print("Removed current .rainy folder.")
 
         if os.path.exists(self.abs_cfg_folder_path):
             print(f"Configuration Folder already exists at '{self.abs_cfg_folder_path}'. Skipping creation. If this is wrong you can recreate this folder by passing --reinit to rainy.py.")
@@ -29,6 +33,8 @@ class Config:
 
         os.mkdir(self.abs_cfg_folder_path)
         print(f"Created .rainy folder at {self.abs_cfg_folder_path}.")
+        os.mkdir(self.abs_cache_folder_path)
+        print(f"Created cache folder at {self.abs_cache_folder_path}.")
         with open(self.abs_cfg_file_path, "w") as file:
             file.write("""# Configuration File for rainy.
 # Any values can be changed using CLI-Arguments as well.
@@ -110,7 +116,15 @@ show_surface_pressure = True
 use_emoji = False
 
 # Specify if the output should contain the ASCII-Art of the corresponding weather, True or False
-show_ascii_art = True""")
+show_ascii_art = True
+
+
+[Misc]
+# This specifies how many seconds can pass before the cache is considered to be expired.
+cache_ttl = 360
+
+# This specifies how many diffrent locations (Cities) can be cached at once before starting to remove the oldest cache and replace it with the new cache for the new city.
+max_cache_file_count = 10""")
         print(f"Created configuration file at '{self.abs_cfg_file_path}'.")
         return None
 
@@ -127,34 +141,54 @@ show_ascii_art = True""")
                 self.remove_directory_tree(path)
         os.rmdir(start_directory)
 
-    def write_cache(self, city: str, cache: str):
-        with open(self.abs_cache_file_path, "w") as file:
-            file.write(f"{city}|{cache}")
+    def get_abs_cache_file_path(self, city) -> str:
+        return os.path.join(self.abs_cache_folder_path, f"cache_{city.replace(" ", "-")}.json")
 
-    def load_cache(self, requested_city: str) -> dict | None:
-        if self.is_cache_present() and self.is_cache_valid():
-            with open(self.abs_cache_file_path, "r") as file:
-                cache = file.read()
-                found_city: str = cache.split("|")[0]
-                if found_city == requested_city:
-                    return json.loads(cache.split("|")[1])
+    def count_cache_files(self) -> int:
+        return len(os.listdir(self.abs_cache_folder_path))
+
+    def get_oldest_cache_file_path(self) -> str:
+        data = []
+        for cache_file in os.listdir(self.abs_cache_folder_path):
+            cache_file_path = os.path.join(self.abs_cache_folder_path, cache_file)
+            modification_time = os.path.getmtime(cache_file_path)
+            data.append([cache_file_path, modification_time])
+
+        data.sort(key=lambda sublist: sublist[1])
+        return data[0][0]
+
+
+    def write_cache(self, city: str, cache: str):
+        while self.count_cache_files() >= self.max_cache_file_count: # While loop needed to adapt if config max_cache_file_count changed blow the current cache file count
+            self.remove_cache_file(self.get_oldest_cache_file_path()) # Removes the oldest cache if max cache count is reached
+
+        with open(self.get_abs_cache_file_path(city), "w") as file:
+            file.write(cache)
+
+    def load_cache(self, city: str) -> dict | None:
+        if self.is_cache_present(city) and self.is_cache_valid(city):
+            with open(self.get_abs_cache_file_path(city), "r") as file:
+                return json.loads(file.read())
         return None
 
-    def is_cache_present(self):
-        if not os.path.exists(self.abs_cache_file_path):
-            print(f"Cache File not found at {self.abs_cache_file_path}")
+    def is_cache_present(self, city):
+        abs_cache_file_path = self.get_abs_cache_file_path(city)
+        if not os.path.exists(abs_cache_file_path):
+            print(f"Cache is empty.", end="\r")
             return False
-        if os.path.getsize(self.abs_cache_file_path) <= 0:
-            print("Cache is empty.")
+        if os.path.getsize(abs_cache_file_path) <= 0:
+            print("Cache file is empty.", end="\r")
             return False
         return True
 
-    def is_cache_valid(self):
-        if (round(time.time()) - round(os.path.getmtime(self.abs_cache_file_path))) <= cache_ttl:
+    def is_cache_valid(self, city):
+        if (round(time.time()) - round(os.path.getmtime(self.get_abs_cache_file_path(city)))) <= self.cache_ttl:
             return True
-        print("Cache is over TimeToLive.")
+        print("Cache has expired...", end="\r")
         return False
 
+    def remove_cache_file(self, path: str):
+        os.remove(path)
 
     def load_config(self):
         parser = configparser.ConfigParser()
@@ -195,5 +229,18 @@ show_ascii_art = True""")
             # Output options
             "use_emoji": parser.getboolean("Output", "use_emoji"),
             "show_ascii_art": parser.getboolean("Output", "show_ascii_art"),
+
+            # Misc
+            "cache_ttl": parser.getint("Misc", "cache_ttl"),
+            "max_cache_file_count": parser.getint("Misc", "max_cache_file_count")
         }
+
+        cache_ttl = cfg.get("cache_ttl")
+        if cache_ttl is not None and cache_ttl.is_integer() and cache_ttl >= 1:
+            self.cache_ttl = round(cache_ttl)
+
+        max_cache_file_count = cfg.get("max_cache_file_count")
+        if max_cache_file_count is not None and max_cache_file_count.is_integer() and max_cache_file_count >= 1:
+            self.max_cache_file_count = round(max_cache_file_count)
+
         return cfg
